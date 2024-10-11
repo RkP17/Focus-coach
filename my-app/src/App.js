fc087c6ac3b5d1ba4302adc6a1c6fb4586a57d1c
Rajveer Patter <raj.patter@outlook.com>
2024-10-11 06:44:42 +0100

    Changed the public comonents
    I changed them back since i couldn't code my page but i have done it in a different branch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import PomodoroTimer from '/Users/rajveerpatter/Documents/GitHub/Focus-coach/my-app/src/Componenets/PomorodoTimer.js';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Importing React Router components
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Welcome to Focus coach </h1>
     </div>
   );
 }
